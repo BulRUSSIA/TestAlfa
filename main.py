5124a0034b68ce7f19b20b4b41ae67c07ac3440f
BulRUSSIA <man31780@gmail.com>
2020-04-26 19:52:25 +0300

    Добавлен ввод периода расчета бонусов

--- main.py
+++ main.py
@@ -4,23 +4,43 @@
 from Models.LocalData import LocalData
 import datetime
 
-in_date = datetime.date(2015, 2, 1)  # период расчета бонусов
-out_date = datetime.datetime(2016, 3, 4)  # период расчета бонусов
-local = LocalData()
-for employ in local.employees:  # парсим сотрудников
-    print('----------------------------------------------')
-    print("Фио сотрудника:", employ.employer_full_name)
-    print("Бонусы сотрудника:", employ.bonus_code)
-    print("Дата устройства на работу:", employ.in_work)
-    print("Дата увольнения:", employ.out_work)
 
-    """Парсим бонусы для сотрудника"""
-    for bonus_id in employ.bonus_code:
-        for bonus_obj in local.bonuses:
-            if bonus_obj.id_ == bonus_id:
-                print("Объект бонус для конкретного сотрудника:", bonus_obj)
+def date_in(date):
+    year, month, day = map(int, date.split('-'))
+    full_date = datetime.date(year, month, day)
 
-    """Парсим контракты для сотрудника"""
-    for contract in local.contracts:
-        if contract.employer_code == employ.id_:
-            print("Контракт сотрудника:", contract)
+    return full_date
+
+
+def main():
+    date_entry = input('Введите начало периода в формате YYYY-MM-DD:')
+    date_end = input('Введите конец периода в формате YYYY-MM-DD:')
+    in_date = date_in(date_entry)  # период расчета бонусов
+    out_date = date_in(date_end)  # период расчета бонусов
+    local = LocalData()  # получаем списки сущностей при обращении к полям класса
+    for employ in local.employees:  # парсим сотрудников
+
+        print('----------------------------------------------')
+        print("Фио сотрудника:", employ.employer_full_name)
+        print("Бонусы сотрудника:", employ.bonus_code)
+        print("Дата устройства на работу:", employ.in_work)
+        print("Дата увольнения:", employ.out_work)
+
+        if employ.out_work is not None and out_date > employ.out_work > in_date:  # Если дата увольнения попадает в
+            # период расчета бонусов
+            print('Бонусы не начисляются')
+
+        """Парсим бонусы для сотрудника"""
+        for bonus_id in employ.bonus_code:
+            for bonus_obj in local.bonuses:
+                if bonus_obj.id_ == bonus_id:
+                    print("Объект бонус для конкретного сотрудника:", bonus_obj)
+
+        """Парсим контракты для сотрудника"""
+        for contract in local.contracts:
+            if contract.employer_code == employ.id_:
+                print("Контракт сотрудника:", contract)
+
+
+if __name__ == '__main__':
+    main()
